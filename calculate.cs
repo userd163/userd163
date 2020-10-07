63f40ad31fe96728103fe81303d9b49e676fa413
userd163 <1439581890@qq.com>
2020-10-07 08:40:09 +0800

    calculate.cs is created

---
+++ calculate.cs
@@ -0,0 +1,21 @@
+public static class caculate
+{
+     public int add(int a,int b)
+     {  returen a+b;
+     }
+     public int max(int a,int b)
+     {
+         if(a>b)
+	 {
+                    return a;
+	     else 
+		     return b;
+	 }
+
+
+     }
+
+
+
+}
+
